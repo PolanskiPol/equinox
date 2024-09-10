036363eb33a2a92addaf7f9387dd27a87f8ced76
PolanskiPol <polanski.real@gmail.com>
2024-09-10 08:49:49 +0200

    feat: Options for multiple controllers in MP games
    This commit also increases version from 4.2 to 4.3

--- addons/equinox/pawn_controller_system/controller.gd
+++ addons/equinox/pawn_controller_system/controller.gd
@@ -9,6 +9,12 @@ extends Node
 signal pawn_posessed(pawn : Node)
 signal pawn_unposessed(pawn : Node)
 
+## The controller where the input is taken from.
+## If multiple controllers are connected to the same machine, only the one with the same [member InputEvent.device] property will count for input.
+## Leave as 0 if your game is Singleplayer.
+## Increase it for every [Controller] in your game if it is Local Multiplayer.
+@export var controller_device : int = 0
+
 ## [Pawn2D] or [Pawn3D] controlled by the Controller.
 @export var pawn : Node :
 	set(new_pawn):
@@ -42,6 +48,14 @@ func unposess() -> void:
 	pawn.free_control()
 	pawn_posessed.emit(pawn)
 	pawn = null
+	
+## Custom is_action_pressed function that evaluates [InputEvent] and [member controller_device]
+func is_action_pressed(event : InputEvent, action : StringName) -> bool:
+	return event.is_action_pressed(action) && event.device == controller_device
+	
+## Custom is_action_released function that evaluates [InputEvent] and [member controller_device]
+func is_action_released(event : InputEvent, action : StringName) -> bool:
+	return event.is_action_released(action) && event.device == controller_device
 	
 ## Virtual function, called on ready.
 ## Override to add your behaviour.
